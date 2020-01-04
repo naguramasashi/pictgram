7656e8ea62bbe27b1409fc7ab38e8f562774ea44
EC2 Default User <ec2-user@ip-172-31-21-126.us-east-2.compute.internal>
2020-01-04 19:22:10 +0000

    homework

--- pictgram/db/migrate/20200104072713_add_comment_to_comment.rb
+++ pictgram/db/migrate/20200104072713_add_comment_to_comment.rb
@@ -1,5 +1,7 @@
 class AddCommentToComment < ActiveRecord::Migration[5.0]
   def change
     add_column :comments, :comment, :text
+    add_column :comments, :topic_id, :integer
+    add_column :comments, :user_id, :integer
   end
 end
--- pictgram/db/schema.rb
+++ pictgram/db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20200104072713) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.text     "comment",    limit: 65535
+    t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
